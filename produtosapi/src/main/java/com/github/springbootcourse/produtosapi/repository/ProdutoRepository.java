614f614a88a6a0410cb78e401e3f99c8e2a269d7
Gabriel Silva <gabriel.silva@squadra.com.br>
2025-08-05 21:28:50 -0300

    Secao 2 - API de Produtos

---
+++ produtosapi/src/main/java/com/github/springbootcourse/produtosapi/repository/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.github.springbootcourse.produtosapi.repository;
+
+import com.github.springbootcourse.produtosapi.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+    List<Produto> findByNome(String nome);
+}
